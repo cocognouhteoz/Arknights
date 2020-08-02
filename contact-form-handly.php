53083f7a5ae6e889a77bd5e986f095445268a886
hung <hungc4k43@gmail.com>
2020-08-02 15:54:29 +0200

    before deploy

---
+++ contact-form-handly.php
@@ -0,0 +1,17 @@
+<?php
+    $name=$_POST['name'];
+    $visitor_email=$_POST['email'];
+    $message=$_POST['message'];
+    $email_from='hungc4k43@gmail.com';
+    $email_subject="New Form Submission";
+    $email_body="User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                         "User Name: $message.\n";
+
+    $to="lephuongthaoc4k43@gmail.com";
+
+    $headers="From $email_from \r \n";
+    $headers="Reply-to: $visitor_email \r \n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: index.html");
+?>
